ea4ee1cd967dec537865779d5b8ccf56dfdccf41
BJ Maynard <linuxmaynard@gmail.com>
2023-02-02 09:18:57 -0500

    Update ConnectAF.ps1

--- ConnectAF.ps1
+++ ConnectAF.ps1
@@ -1,9 +1,9 @@
 ﻿$connectTestResult = Test-NetConnection -ComputerName nordicaf.file.core.windows.net -Port 445
 if ($connectTestResult.TcpTestSucceeded) {
     # Save the password so the drive will persist on reboot
-    cmd.exe /C "cmdkey /add:`"nordicaf.file.core.windows.net`" /user:`"localhost\nordicaf`" /pass:`"K1gAsPHbsIhLCYpe9ZUO6T783KhEyy+ehrtWlZ4tENviaC+OjvbwMXtLp3r014nnlje7+YDM5UiF+AStRvWnqg==`""
+    cmd.exe /C "cmdkey /add:`"companynameaf.file.core.windows.net`" /user:`"localhost\companynameaf`" /pass:`"paste you key in here`""
     # Mount the drive
-    New-PSDrive -Name Z -PSProvider FileSystem -Root "\\nordicaf.file.core.windows.net\tempshare" -Persist
+    New-PSDrive -Name Z -PSProvider FileSystem -Root "\\companynameaf.file.core.windows.net\tempshare" -Persist
 } else {
     Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
 }
